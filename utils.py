8a9176154a5bf1e0a3205ae805b2d7576ef990b6
Lena Yu <56573422+papafranco69@users.noreply.github.com>
2023-03-22 19:05:08 +0900

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import fetch_openml
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+
+X, y = mnist["data"], mnist["target"]
+
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28,28)
+
+plt.imshow(some_digit_image, cmap=mpl.cm.binary, interpolation="nearest")
+plt.axis("off")
+plt.show()
